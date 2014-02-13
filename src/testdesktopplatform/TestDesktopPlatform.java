cfa6a0921128947e111d613b3ffe159a304125a0
tog <tog@CPH-TOG.efif.dk>
2014-02-13 11:37:41 +0100

    Initial Commit

---
+++ src/testdesktopplatform/TestDesktopPlatform.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ *//*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package testdesktopplatform;
+
+import applicationapi.Application;
+import applicationapi.Platform;
+import desktopplatform.DesktopPlatformImpl;
+import exampleapplication.ExampleApplication;
+
+
+
+/**
+ *
+ * @author tog
+ */
+public class TestDesktopPlatform
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        Application app = new ExampleApplication();
+        Platform p = new DesktopPlatformImpl();
+        
+        p.runApplication(app);
+    }
+    
+}
